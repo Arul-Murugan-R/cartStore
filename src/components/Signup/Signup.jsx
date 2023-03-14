fa22e400e73912c62b034aa29aef394e62784771
Arul_Murugan_R <arulmuruganarjun14@gmail.com>
2023-03-14 23:00:46 +0530

    signup

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,35 +1,83 @@
+import { useState } from 'react'
+
 import './Signup.css'
 const Signup = () => {
-
+    const [signupData, setSignupData] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
+    const [errors, setErrors] = useState()
+    const onBlurHandler = (e) => {
+        setErrors(null)
+        const { name, value } = e.target
+        setSignupData({ ...signupData, [name]: value })
+        console.log(signupData)
+    }
     const submitHandler = (e) => {
         e.preventDefault()
-        console.log("Submitted")
+        setIsLoading(true)
+        fetch(import.meta.env.VITE_BACKEND + '/auth/signup', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(signupData)
+        }).then((res) => {
+            console.log(res)
+            if (res.status === 'ok'||res.status === 201) {
+                setErrors(null)
+                setIsLoading(false)
+                setSignupData({})
+                window.location = '/auth/login'
+            }
+            return res.json()
+        }).then((data) => {
+            console.log(data)
+            if (data.message) {
+                setIsLoading(false)
+                setErrors(data.message+" "+data.output)
+            }
+        }).catch((err) => {
+            console.log(err)
+            setIsLoading(false)
+            setErrors(err)
+        })
+        
     }
+    if (isLoading) {
+        return (
+            <div class="d-flex justify-content-center">
+                <div class="spinner-border" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>
+        )
+    }
+            
 
     return (
         <div className="signup-form">
-            <form action="/auth/signup" onSubmit={submitHandler} novalidate>
+            {errors&&<div className="alert alert-danger" role="alert">{errors}</div>}
+            <form action="/auth/signup" onSubmit={submitHandler} className='bg-light'>
                 <div className="form-header">
                     <h2>Sign Up</h2>
                 </div>
                 <div className="form-group">
                     <label>Username</label>
-                    <input type="text" className="form-control" name="name" required="required" />
+                    <input type="text" className="form-control" name="name" value={errors&&signupData.name} onBlur={onBlurHandler} required="required" />
                 </div>
                 <div className="form-group">
                     <label>Email Address</label>
-                    <input type="email" className="form-control" name="email" required="required" />
+                    <input type="email" className="form-control" name="email" value={errors&&signupData.email} onBlur={onBlurHandler} required="required" />
                 </div>
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" name="password" required="required" />
+                    <input type="password" className="form-control" name="password" onBlur={onBlurHandler} required="required" />
                 </div>
                 <div className="form-group">
                     <label>Confirm Password</label>
-                    <input type="password" className="form-control" name="cpassword" required="required" />
+                    <input type="password" className="form-control" name="cpassword"  onBlur={onBlurHandler} required="required" />
                 </div>
                 <div className="form-group">
-                    <label className="checkbox-inline"><input type="checkbox" required="required" /> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
+                    <label className="checkbox-inline"><input type="checkbox" /> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
                 </div>
                 <div className="form-group">
                     <button type="submit" className="btn btn-primary btn-block btn-lg">Sign Up</button>
