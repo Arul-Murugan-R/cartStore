54912ee455640d7b574477e573d3ec9921d4c1c3
Arul_Murugan_R <arulmuruganarjun14@gmail.com>
2023-03-17 22:59:01 +0530

    isLoggedIn?

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,19 @@
 import { Link } from 'react-router-dom';
-const Navbar = () => {
+import { useState, useEffect } from 'react';
+const Navbar = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const logoutHandler = () => {
+    localStorage.clear();
+    setIsLoggedIn(false);
+  };
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+
     return(
         
 <header className="nav-bar p-3 bg-dark text-white sticky-md-top">
@@ -9,11 +23,11 @@ const Navbar = () => {
           <img className="logo" src="https://res.cloudinary.com/da3xs5vzk/image/upload/v1676896905/logo6_kqpwf1.ico" />
         </Link>
         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-lg-center mb-md-0">
-          <li><Link to="/home" className="nav-link px-2 text-white">Home</Link></li>
+        <li><Link to="/home" className={`nav-link px-2 ${props.path=='/home'?'text-secondary':'text-white'}`}>Home</Link></li>
           
-          <li><Link to="/add-yours" className="nav-link px-2 text-white">Add Yours</Link></li>
+          <li><Link to="/add-yours" className={`nav-link px-2 ${props.path=='/add-yours'?'text-secondary':'text-white'}`}>Add Yours</Link></li>
           
-          <li><Link to="/product" className="nav-link px-2 text-white">My List</Link></li>
+          {isLoggedIn&&<li><Link to="/product" className={`nav-link px-2 ${props.path=='/product'?'text-secondary':'text-white'}`}>My List</Link></li>}
          
           <li><Link to="#" className="nav-link px-2 text-white">FAQs</Link></li>
           <li><Link to="#" className="nav-link px-2 text-white">About</Link></li>
@@ -34,10 +48,14 @@ const Navbar = () => {
         {/* <div className="text-end">
           <Link to="/auth/logout" className="btn btn-outline-light me-2">Logout</Link>
         </div> */}
+        {!isLoggedIn ? (
         <div className="text-end">
           <Link to="/auth/login" className="btn btn-outline-light me-2">Login</Link>
           <Link to="/auth/signup" type="button" className="btn btn-warning">Sign-up</Link>
-        </div>
+        </div>) : (
+        <div className="text-end">
+          <Link onClick={logoutHandler} className="btn btn-outline-light me-2">Logout</Link>
+      </div>)}
       </div>
     </div>
   </header>
