52d5fbef797d8143917525e1c243685283555db2
Arul_Murugan_R <arulmuruganarjun14@gmail.com>
2023-04-07 11:56:55 +0530

    err

--- src/components/Addyours.jsx
+++ src/components/Addyours.jsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom'
 import { useNavigate } from "react-router-dom";
 
 const Addyours = () => {
-
+  const [error,setError] = useState(false)
 
     return(
         <>
